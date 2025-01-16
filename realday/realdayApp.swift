426685e58901dc1eede86c22c8de945220d5628e
Johann Petzold <johann.petzold@gmail.com>
2025-01-16 19:34:58 +0100

    Initial Commit

---
+++ realday/realdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  realdayApp.swift
+//  realday
+//
+//  Created by Johann Petzold on 16/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct realdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ realdayTests/realdayTests.swift
@@ -0,0 +1,17 @@
+//
+//  realdayTests.swift
+//  realdayTests
+//
+//  Created by Johann Petzold on 16/01/2025.
+//
+
+import Testing
+@testable import realday
+
+struct realdayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
